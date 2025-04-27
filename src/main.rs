b7c605c7618593fd02b412716f281716b31638ab
Daniel Kantor <git@daniel-kantor.com>
2025-04-27 19:52:21 +0200

    add basic search functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use std::path::Path;
+
+use clap::Parser;
+use grep::{
+    regex::RegexMatcher,
+    searcher::{SearcherBuilder, sinks::UTF8},
+};
+use ignore::WalkBuilder;
+
+/// Search frecently edited code in a Git repository
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Regular Expression pattern
+    #[arg()]
+    pattern: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let matcher = RegexMatcher::new(&args.pattern).expect("Invalid regular expressoin");
+    let mut searcher = SearcherBuilder::new().line_number(true).build();
+
+    let root = Path::new(".");
+
+    for result in WalkBuilder::new(root).build() {
+        let entry = result.expect("Failed to read directory entry");
+        if entry.file_type().map(|ft| ft.is_file()).unwrap_or(false) {
+            let path = entry.path();
+            if let Err(err) = searcher.search_path(
+                &matcher,
+                path,
+                UTF8(|lnum, line| {
+                    print!("{}:{}: {}", path.display(), lnum, line);
+                    Ok(true)
+                }),
+            ) {
+                eprintln!("Failed to search {}: {}", path.display(), err);
+            }
+        }
+    }
 }
